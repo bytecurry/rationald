1b6d88ddb30bb222abba83a2b849429ad42d65ae
Thayne McCombs <astrothayne@gmail.com>
2016-03-07 23:56:54 -0700

    Add some unit tests

--- source/bytecurry/rational.d
+++ source/bytecurry/rational.d
@@ -34,6 +34,14 @@ public:
         den = d;
         normalize();
     }
+    unittest {
+        auto a = rational(1,2);
+        assert(a.numerator == 1);
+        assert(a.denominator == 2);
+        a = rational(10,25);
+        assert(a.numerator == 2);
+        assert(a.denominator == 5);
+    }
 
     this(I: T)(I n) pure nothrow {
         num = n;
@@ -50,11 +58,31 @@ public:
     @property T denominator() pure nothrow const {
         return den;
     }
+
+    /**
+     * Return if the rational is infinite. i.e. if the denominator is zero and the numerator
+     * is non-zero
+     */
     @property bool isInfinite() pure nothrow const {
         return den == 0 && num != 0;
     }
+    unittest {
+        assert(rational(1,0).isInfinite);
+        assert(!rational(0,0).isInfinite);
+        assert(!rational(0,1).isInfinite);
+    }
+
+    /**
+     * Return whether or not the rational is indeterminate (not a number), i.e. both the numerator and
+     * denominator are zero.
+     */
     @property bool isNaN() pure nothrow const {
         return den == 0 && num == 0;
+    }
+    unittest {
+        assert(rational(0,0).isNaN);
+        assert(!rational(1,0).isNaN);
+        assert(!rational(0,1).isNaN);
     }
 
     // Assignment Operators:
@@ -80,9 +108,17 @@ public:
         return num == other.num && den == other.den;
     }
 
+    unittest {
+        assert(rational(1,2) == rational(2,4));
+    }
+
     ///
     bool opEquals(I: T)(I other) pure nothrow const {
         return num == other && den == 1;
+    }
+
+    unittest {
+        assert(rational(5,1) == 5);
     }
 
     ///
@@ -90,9 +126,21 @@ public:
         return num * other.den - other.num * den;
     }
 
+    unittest {
+        assert(rational(2,3) > rational(1,2));
+        assert(rational(4,5) < rational(3,2));
+        assert(rational(3,2) >= rational(3,2));
+    }
+
     ///
     int opCmp(I: T)(I other) pure nothrow const @nogc {
         return num - other* den;
+    }
+
+    unittest {
+        assert(rational(3,2) > 1);
+        assert(rational(1,2) < 1);
+        assert(rational(4,4) >= 1);
     }
 
     // Unary Operators:
@@ -105,6 +153,11 @@ public:
     ///
     Rational opUnary(string op)() const pure nothrow if (op == "-") {
         return Rational(-num, den);
+    }
+
+    unittest {
+        assert(-rational(1,2) == rational(-1,2));
+        assert(-rational(-1,2) == rational(1,2));
     }
 
     // Binary Operators:
@@ -130,12 +183,21 @@ public:
         return (cast(F) this) ^^ other;
     }
 
+    unittest {
+        assert(rational(1,4) ^^ 0.5 == 0.5);
+    }
+
     // int + rational, and int * rational
     ///
     Rational!(CommonType!(T,U)) opBinaryRight(string op, U)(U other) const pure nothrow
     if (op == "+" || op == "*")
     {
         return opBinary!(op)(other);
+    }
+
+    unittest {
+        assert(1 + rational(3,2) == rational(5,2));
+        assert(2 * rational(1,2) == rational(1,1));
     }
 
     ///
@@ -145,11 +207,19 @@ public:
         return Rational(other * den - num, den);
     }
 
+    unittest {
+        assert(2 - rational(1,2) == rational(3,2));
+    }
+
     ///
     Rational!(CommonType!(T,U)) opBinaryRight(string op, U)(U other) const pure nothrow
     if (op == "/")
     {
-        return Rational(num, other * den);
+        return Rational(other * den, num);
+    }
+
+    unittest {
+        assert(3 / rational(2,3) == rational(9,2));
     }
 
 
